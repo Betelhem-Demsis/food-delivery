4a0bf1b1648b47b32c8494ba61fe545f1a196c29
betelhem demsis <bettydemsis@gmail.com>
2024-06-07 02:22:43 +0300

    update

--- src/Popular.js
+++ src/Popular.js
@@ -1,12 +1,50 @@
 import React from "react";
-import food from "./assets/image/th.jpeg";
-import food2 from "./assets/image/photo_2024-01-28_10-48-07.jpg";
-import food3 from "./assets/image/th (3).jpeg";
-import food4 from "./assets/image/photo_2024-01-28_10-51-21.jpg";
-import food5 from "./assets/image/imani-manyara-Bfy8z7EOyx4-unsplash.jpg";
-import food6 from "./assets/image/th (4).jpeg";
-
 import "./assets/CSS/popular.css";
+
+const Popularitems = [
+  {
+    imageName: require("./assets/image/th.jpeg"),
+    name: "Pizza",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent gravida semper magna, non finibus libero rhoncus et. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae",
+    price: "Birr 300.00",
+  },
+  {
+    imageName: require("./assets/image/photo_2024-01-28_10-51-21.jpg"),
+    name: "Kitfo",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent gravida semper magna, non finibus libero rhoncus et. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae",
+    price: "Birr 630.00",
+  },
+  {
+    imageName: require("./assets/image/th (3).jpeg"),
+    name: "Burger",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent gravida semper magna, non finibus libero rhoncus et. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae",
+    price: "Birr 450.00",
+  },
+  {
+    imageName: require("./assets/image/photo_2024-01-28_10-48-30.jpg"),
+    name: "Pasta",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent gravida semper magna, non finibus libero rhoncus et. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae",
+    price: "Birr 130.00",
+  },
+  {
+    imageName: require("./assets/image/imani-manyara-Bfy8z7EOyx4-unsplash.jpg"),
+    name: "Chips",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent gravida semper magna, non finibus libero rhoncus et. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae",
+    price: "Birr 200.00",
+  },
+  {
+    imageName: require("./assets/image/th (4).jpeg"),
+    name: "Black Forest",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent gravida semper magna, non finibus libero rhoncus et. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae",
+    price: "Birr 120.00",
+  },
+];
 
 function Popular() {
   return (
@@ -16,102 +54,19 @@ function Popular() {
         <p>Choose Your Favourite food among these Top 6 dishes</p>
       </div>
       <div className="popular_foods">
-        <div className="popular_food">
-          <img src={food} alt="" />
-          <div className="food_detail">
-            <h3>Pizza</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-              gravida semper magna, non finibus libero rhoncus et. Vestibulum
-              ante ipsum primis in faucibus orci luctus et ultrices posuere
-              cubilia curae;
-            </p>
-            <div className="money_pop">
-              <h4>Birr 300.00</h4>
-              <button>Order Now</button>
+        {Popularitems.map((popular) => (
+          <div className="popular_food" key={popular.name}>
+            <img src={popular.imageName} alt={popular.name} />
+            <div className="food_detail">
+              <h3>{popular.name}</h3>
+              <p>{popular.description}</p>
+              <div className="money_pop">
+                <h4>{popular.price}</h4>
+                <button>Order Now</button>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="popular_food">
-          <img src={food4} alt="" />
-          <div className="food_detail">
-            <h3>Kitfo</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-              gravida semper magna, non finibus libero rhoncus et. Vestibulum
-              ante ipsum primis in faucibus orci luctus et ultrices posuere
-              cubilia curae;
-            </p>
-            <div className="money_pop">
-              <h4>Birr 630.00</h4>
-              <button>Order Now</button>
-            </div>
-          </div>
-        </div>
-        <div className="popular_food">
-          <img src={food3} alt="" />
-          <div className="food_detail">
-            <h3>Burger</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-              gravida semper magna, non finibus libero rhoncus et. Vestibulum
-              ante ipsum primis in faucibus orci luctus et ultrices posuere
-              cubilia curae;
-            </p>
-            <div className="money_pop">
-              <h4>Birr 400.00</h4>
-              <button>Order now</button>
-            </div>
-          </div>
-        </div>
-        <div className="popular_food">
-          <img src={food2} alt="" />
-          <div className="food_detail">
-            <h3>Cake</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-              gravida semper magna, non finibus libero rhoncus et. Vestibulum
-              ante ipsum primis in faucibus orci luctus et ultrices posuere
-              cubilia curae;
-            </p>
-            <div className="money_pop">
-              <h4>Birr 130.00</h4>
-              <button>Order Now</button>
-            </div>
-          </div>
-        </div>
-        <div className="popular_food">
-          <img src={food5} alt="" />
-          <div className="food_detail">
-            <h3>Chips</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-              gravida semper magna, non finibus libero rhoncus et. Vestibulum
-              ante ipsum primis in faucibus orci luctus et ultrices posuere
-              cubilia curae;
-            </p>
-            <div className="money_pop">
-              <h4>Birr 200.00</h4>
-              <button>Order Now</button>
-            </div>
-          </div>
-        </div>
-        <div className="popular_food">
-          <img src={food6} alt="" />
-          <div className="food_detail">
-            <h3>Black Forest</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-              gravida semper magna, non finibus libero rhoncus et. Vestibulum
-              ante ipsum primis in faucibus orci luctus et ultrices posuere
-              cubilia curae;
-            </p>
-            <div className="money_pop">
-              <h4>Birr 120.00</h4>
-              <button>Order Now</button>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
